bdc9289a27c479298719ca8bab68f1b814966768
anilac13 <anil4u2496@gmail.com>
2023-04-29 18:52:04 +0530

    [Anil] Add:Print Fibonacci series for given number

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        public static void PrintFibonacciSeries()
+        {
+            Console.WriteLine("Enter number to print Fibonacci series");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int firstNum = 0, secondNum = 1, thirdNum = 0;
+            Console.Write("Fibonacci series is: " + firstNum + " " + secondNum);
+            for (int i = 2; i < num; i++)
+            {
+                thirdNum = firstNum + secondNum;
+                Console.Write(" " + thirdNum);
+                firstNum = secondNum;
+                secondNum = thirdNum;
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -5,7 +5,21 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Logical programs");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nChoose option to perform \n1.Print Fibonacci series \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries.PrintFibonacciSeries();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
