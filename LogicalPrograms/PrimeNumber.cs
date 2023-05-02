834c6abf67c5241508004cbba41082baa3793873
anilac13 <anil4u2496@gmail.com>
2023-05-02 18:11:11 +0530

    [Anil] Add:Check whether the given number is Prime or not

---
+++ LogicalPrograms/PrimeNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class PrimeNumber
+    {
+        public static void FindPrimeNumber()
+        {
+            Console.WriteLine("Enter any number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            bool Is_Prime = true;
+            int i;
+            for (i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Is_Prime = false;
+                    break;
+                }
+                else
+                {
+                    Is_Prime = true;
+                }
+            }
+            if (Is_Prime == true)
+                Console.WriteLine("{0} is Prime Number", num);
+            else
+                Console.WriteLine("{0} is not Prime Number", num);
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -8,7 +8,7 @@ namespace LogicalPrograms
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nChoose option to perform \n1.Print Fibonacci series \n2.Perfect number \n3.Exit");
+                Console.WriteLine("\nChoose option to perform \n1.Print Fibonacci series \n2.Perfect number \n3.Prime number \n4.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -19,6 +19,9 @@ namespace LogicalPrograms
                         PerfectNumber.CheckPerfectNumber();
                         break;
                     case 3:
+                        PrimeNumber.FindPrimeNumber();
+                        break;
+                    case 4:
                         flag = false;
                         break;
                 }
