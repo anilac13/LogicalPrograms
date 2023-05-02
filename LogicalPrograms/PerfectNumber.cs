8a101d1043affdc41e8867abfb9933e127c4368c
anilac13 <anil4u2496@gmail.com>
2023-05-02 08:24:31 +0530

    [Anil] Add:Print Perfect numbers in a given range

---
+++ LogicalPrograms/PerfectNumber.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class PerfectNumber
+    {
+        public static void CheckPerfectNumber()
+        {
+            //Console.WriteLine("Enter any number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //int result = 0;
+            //for (int i = 1; i < num; i++)
+            //{
+            //    if (num % i  == 0)
+            //    {
+            //        result += i;
+            //    }
+            //}
+            //if (result == num)
+            //{
+            //    Console.WriteLine("{0} is a Perfect number", num);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("{0} is not a Perfect number", num);
+            //}
+
+            Console.WriteLine("Print Perfect numbers in given range");
+            Console.WriteLine("Enter starting range");
+            int mn = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter ending range");
+            int mx = Convert.ToInt32(Console.ReadLine());
+            for (int n = mn; n <= mx; n++)
+            {
+                int sum = 0;
+                int i = 1;
+                while (i < n)
+                {
+                    if (n % i == 0)
+                        sum += i;
+                    i++;
+                }
+                if (sum == n)
+                    Console.WriteLine(n);
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -8,7 +8,7 @@ namespace LogicalPrograms
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nChoose option to perform \n1.Print Fibonacci series \n2.Exit");
+                Console.WriteLine("\nChoose option to perform \n1.Print Fibonacci series \n2.Perfect number \n3.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -16,6 +16,9 @@ namespace LogicalPrograms
                         FibonacciSeries.PrintFibonacciSeries();
                         break;
                     case 2:
+                        PerfectNumber.CheckPerfectNumber();
+                        break;
+                    case 3:
                         flag = false;
                         break;
                 }
